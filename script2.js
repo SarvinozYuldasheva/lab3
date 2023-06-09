1639b28afb8ab9b9a4230dee91fa918da67c72de
SarvinozYuldasheva <131155341+SarvinozYuldasheva@users.noreply.github.com>
2023-06-09 12:21:18 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,6 @@
+
+
+function admin(){
+    document.getElementById("admin").innerText= `Admin: ${document.getElementById('name').value}`//интерполяция
+    // "Admin: " + document.getElementById("name").value;
+    }
